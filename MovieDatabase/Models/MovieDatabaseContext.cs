3cd04ddc5dc7d6d38581d4f42604d49e67f6c6f4
noahkise <noah@kisefamily.com>
2024-02-05 09:28:03 -0800

    add to file structure, boilerplate code
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

---
+++ MovieDatabase/Models/MovieDatabaseContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieDatabase.Models;
+
+public class MovieDatabaseContext : DbContext
+{
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Film> Films { get; set; }
+    public DbSet<ActorFilm> ActorFilms { get; set; }
+
+    public MovieDatabaseContext(DbContextOptions options) : base(options) { }
+}
