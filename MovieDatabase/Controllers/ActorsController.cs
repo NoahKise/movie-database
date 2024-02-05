492eeefc49c11844f3da037bb089e8dd04a07b5a
noahkise <noah@kisefamily.com>
2024-02-05 12:00:42 -0800

    full CRUD for actor, lunch
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- MovieDatabase/Controllers/ActorsController.cs
+++ MovieDatabase/Controllers/ActorsController.cs
@@ -19,4 +19,53 @@ public class ActorsController : Controller
         List<Actor> model = _db.Actors.ToList();
         return View(model);
     }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Actor actor)
+    {
+        _db.Actors.Add(actor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+        Actor thisActor = _db.Actors
+       .FirstOrDefault(actor => actor.ActorId == id);
+        return View(thisActor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        Actor thisActor = _db.Actors.FirstOrDefault(actor => actor.ActorId == id);
+        return View(thisActor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Actor actor)
+    {
+        _db.Actors.Update(actor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        Actor thisActor = _db.Actors.FirstOrDefault(actor => actor.ActorId == id);
+        return View(thisActor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Actor thisActor = _db.Actors.FirstOrDefault(actor => actor.ActorId == id);
+        _db.Actors.Remove(thisActor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
